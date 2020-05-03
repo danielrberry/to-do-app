549db3fe70a0edb2be42fad4473ee270dfa70bf7
danielrberry <53920147+danielrberry@users.noreply.github.com>
2020-05-03 10:39:31 -0500

    create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  alert("The window has loaded!");
+};
+
+ window.onload = function() {
+  onReady();
+ };
+ 
+ function onReady() {
+ const addToDoForm = document.getElementById('addToDoForm');
+ const newToDoText = document.getElementById('newToDoText');
+ const toDoList = document.getElementById('toDoList');
+ 
+ addToDoForm.addEventListener('submit', () => {
+
+  });
+
+}
+
+ window.onload = function() {
+ 
